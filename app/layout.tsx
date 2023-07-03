a7df1ea2b1cc227a7c15a1cb080d8a3bac1326a3
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-07-03 13:13:32 -0400

    form start

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-200`}>
+        <div className='max-w-4xl mx-auto'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
---
+++ app/restaurants/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Form from '@/components/Form'
+import React from 'react'
+
+export default function SingleRestaurantPage() {
+    return (
+        <main className='w-full '>
+            Single Restaurant Page
+            <section className='mx-auto'>
+                <Form />
+            </section>
+        </main>
+    )
+}
