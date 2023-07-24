8f56daab867d317cfc0f23c5a867e32602417998
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-07-24 15:35:53 -0400

    Update Featured.tsx

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -5,17 +5,60 @@ import React, { useEffect, useState } from 'react'
 
 export default function Featured() {
     const [featured, setFeatured] = useState();
+    let data: any;
     const supabase = useSupabaseClient();
 
     useEffect(() => {
-        supabase.from('ratings')
+        supabase.from('restaurants')
             .select(`
+                title,
                 id,
-                filling,
-                parent ( id, title )
+                ratings (
+                    filling,
+                    wrap,
+                    ratio,
+                    accoutrement,
+                    crisp
+                )
             `)
-            .then(result => console.log(result))
-    }, [])
+            .order('id', { foreignTable: 'ratings', ascending: false })
+            .then(result => {
+                data = result.data
+                console.log(data)
+
+                const calculateAverageScores = (restaurant: any) => {
+                    const ratingsCount = restaurant.ratings.length;
+                    const scoresSum = restaurant.ratings.reduce((acc: any, rating: any) => {
+                        const totalScore = Object.values(rating).reduce((sum: any, score: any) => sum + score, 0);
+                         return acc + totalScore;
+                    }, 0);
+                    return scoresSum / (Object.keys(restaurant.ratings[0]).length * ratingsCount);
+                };
+
+                const findHighestRatedRestaurant = (data: any) => {
+                    if (!data || data.length === 0) return null;
+                  
+                    let highestRatedRestaurant = null;
+                    let highestAverageScore = -Infinity;
+                  
+                    data.forEach((restaurant: any) => {
+                      const averageScore = calculateAverageScores(restaurant);
+                      if (averageScore > highestAverageScore) {
+                        highestAverageScore = averageScore;
+                        highestRatedRestaurant = restaurant;
+                      }
+                    });
+                  
+                    return highestRatedRestaurant;
+                  };
+                  
+                  const highestRatedRestaurant = findHighestRatedRestaurant(data);
+                  
+                  console.log("Highest Rated Restaurant:", highestRatedRestaurant);
+                  
+            });
+
+    }, [supabase])
 
     return (
         <div>Featured</div>
